c235605e921ff7d6acdb699cf888cc35c8ad2355
Alam Rodriguez <alamrd2016@gmail.com>
2023-07-24 09:24:50 -0400

    arreglo al ver preview de articulo9

--- src/components/menu-articles/menu-components/PreviewInfoArticle.jsx
+++ src/components/menu-articles/menu-components/PreviewInfoArticle.jsx
@@ -34,9 +34,9 @@ const PreviewInfoArticle = ({setViewPreviewInfoArticle, setViewOrderSelectArticl
 
   if( articleSelected.complex ){
     return (
-      <div className={`z-2 animate__animated ${!close ? 'animate__fadeInUp' : 'animate__fadeOutDown'} vw-100 vh-100 position-fixed position-sticky- start-0 top-0`}>
+      <div className={`d-flex justify-content-center align-items-end z-2 animate__animated ${!close ? 'animate__fadeInUp' : 'animate__fadeOutDown'} vw-100 vh-100 position-fixed position-sticky- start-0 top-0`}>
         
-        <div className=' animate__animated animate__fadeInUp z-3 bg-white overflow-y-scroll position-fixed bottom-0 start-50 border translate-middle-x mb-3 rounded-3 shadow p-4 d-flex flex-column justify-content-between' style={{width:'95%', height:'52%'}}>
+        <div className='animate__animated animate__fadeInUp z-3 bg-white overflow-y-scroll border mb-3 rounded-3 shadow p-4 d-flex flex-column justify-content-between' style={{width:'95%', height:'52%'}}>
             <div className=''>
               <h3 className='fw-bold fs-2 mb-0'>{articleSelected.titulo}</h3>
               <div className='my-4'>
@@ -54,6 +54,27 @@ const PreviewInfoArticle = ({setViewPreviewInfoArticle, setViewOrderSelectArticl
           
       </div>
     );
+    // return (
+    //   <div className={`z-2 animate__animated ${!close ? 'animate__fadeInUp' : 'animate__fadeOutDown'} vw-100 vh-100 position-fixed position-sticky- start-0 top-0`}>
+        
+    //     <div className=' animate__animated animate__fadeInUp z-3 bg-white overflow-y-scroll position-fixed bottom-0 start-50 border translate-middle-x mb-3 rounded-3 shadow p-4 d-flex flex-column justify-content-between' style={{width:'95%', height:'52%'}}>
+    //         <div className=''>
+    //           <h3 className='fw-bold fs-2 mb-0'>{articleSelected.titulo}</h3>
+    //           <div className='my-4'>
+    //             { articleSelected.precios.map( (precio, i) => (
+    //                 <div key={i} className='border-bottom d-flex justify-content-between py-3' onClick={()=>handleClickPrecio(precio)}>
+    //                   <p className='fs-4 m-0'>- {precio.sizeArticle}"</p>
+    //                   <p className='fs-5 m-0 text-secondary'>RD$ {precio.sizeArticlePrice}</p>
+    //                 </div>
+    //               ))
+    //             }
+    //           </div>
+    //         </div>
+    //         <button className={`btn form-control fs-3 ${color1.textColor}`} onClick={handleClickCancelar}>Cancelar</button>
+    //       </div>
+          
+    //   </div>
+    // );
   }else {
     return <></>;
   }
