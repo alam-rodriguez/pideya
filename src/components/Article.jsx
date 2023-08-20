bbcd70456d57565bc94a01d66bfffdd7c6cec86c
Alam Rodriguez <alamrd2016@gmail.com>
2023-08-20 17:46:28 -0400

    arreglando scroll 47

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -40,13 +40,15 @@ const Article = ({viewArticleSelected, setViewArticleSelected}) => {
   if(articleSeleted != null){
     return (
       <main className='overflow-hidden-'>
-        <div className={`animate__animated ${viewArticleSelected=='abrir' ? 'animate__slideInRight' : viewArticleSelected=='cerrar' ? 'animate__slideOutRight' : ''}  container-fluid- p-0- position-fixed start-0 top-0 h-100 w-100 bg-light z-3`}>
-          <ImCancelCircle className='position-absolute text-white display-3' style={{top:20, left:20}} onClick={handleClickBack} />
-          { imgUrl != null
-            ? <img className='w-100 object-fit-cover ' src={imgUrl} style={{height:'78%'}} />
-            : <></>
-          }
-          <div className='d-flex flex-column position-fixed bottom-0 justify-content-between h-25 bg-white rounded-top-5 bottom-0 w-100 p-4 pt-0 shadow-lg'>
+        <div className={`animate__animated ${viewArticleSelected=='abrir' ? 'animate__slideInRight' : viewArticleSelected=='cerrar' ? 'animate__slideOutRight' : ''}  container-fluid- p-0- position-fixed start-0 top-0 h-100 w-100 bg-light z-3`} >
+          <div style={{height:'78vh'}}>
+            <ImCancelCircle className='position-absolute text-white display-3' style={{top:20, left:20}} onClick={handleClickBack} /> 
+            { imgUrl != null
+              ? <img className='w-100 h-100 object-fit-cover ' src={imgUrl}  />
+              : <></>
+            }
+          </div>
+          <div className='d-flex flex-column position-fixed bottom-0 start-0 justify-content-between h-25- bg-white rounded-top-5 bottom-0 w-100 p-4 pt-0 shadow-lg' style={{height:'32vh'}}>
               <div className='align-self-center mt-2 bg-secondary rounded-5' style={{height:4, width:40}}></div>
               <h3 className='fw-bold fs-2 mt-3'>{articleSeleted.title}</h3>
               <p className='fs-6 fw-medium overflow-y-scroll over'>{articleSeleted.subTitle}</p>
