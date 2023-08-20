d7f8fe115e8ceb4ae50a54677f2f170c900f8771
Alam Rodriguez <alamrd2016@gmail.com>
2023-08-20 19:57:27 -0400

    arreglando scroll 50

--- src/components/home/show-code-user/ShowCodeUser.jsx
+++ src/components/home/show-code-user/ShowCodeUser.jsx
@@ -25,22 +25,24 @@ const ShowCodeUser = ({viewCodeUser, setViewCodeUser}) => {
   const handleClickDone = () => setViewCodeUser('close')
 
   return (
-    <div className={`animate__animated ${viewCodeUser == 'open' ? 'animate__slideInRight' : viewCodeUser == 'close' ? 'animate__slideOutRight' : ''} container-fluid vh-100 vw-100 position-fixed bg-white top-0 start-0 z-3`}>
-      <section className='mx-4 d-flex flex-column justify-content-between py-4' style={{height:'70vh'}}>
+    <main className='position-fixed top-0'>
+        <div className={`animate__animated ${viewCodeUser == 'open' ? 'animate__slideInRight' : viewCodeUser == 'close' ? 'animate__slideOutRight' : ''} container-fluid vh-100 vw-100  bg-white top-0 start-0 z-3`}>
+        <section className='mx-4 d-flex flex-column justify-content-between py-4' style={{height:'70vh'}}>
 
-        <h1 className='fw-bold text-center display-1 text-danger'>{appInfo.nombre}</h1>
-        
-        <div className='border border-secondary rounded-5 d-flex flex-column justify-content-center align-items-center gap-3 p-4'>
-          <QRCode value='https://falconmasters.com' className='w-75 h-auto' />
-          <p className='text-center mb-0'>Muestrale este codigo en Pizza Mia al pedir</p>
-          <p className='text-center fw-bold fs-4 mb-0'>{codeUser}</p>
-        </div>
+          <h1 className='fw-bold text-center display-1 text-danger'>{appInfo.nombre}</h1>
+          
+          <div className='border border-secondary rounded-5 d-flex flex-column justify-content-center align-items-center gap-3 p-4'>
+            <QRCode value='https://falconmasters.com' className='w-75 h-auto' />
+            <p className='text-center mb-0'>Muestrale este codigo en Pizza Mia al pedir</p>
+            <p className='text-center fw-bold fs-4 mb-0'>{codeUser}</p>
+          </div>
 
-        <div className='position-fixed bottom-0 start-0 w-100 p-4'>
-          <button className={`btn ${color1.btn} text-center form-control fs-4 p-3`} onClick={handleClickDone}>Hecho</button>
-        </div>
-      </section>
-    </div>
+          <div className='position-fixed bottom-0 start-0 w-100 p-4'>
+            <button className={`btn ${color1.btn} text-center form-control fs-4 p-3`} onClick={handleClickDone}>Hecho</button>
+          </div>
+        </section>
+      </div>
+    </main>
   )
 }
 
